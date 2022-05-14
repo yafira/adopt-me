f864b45b855d3cd33b28a0133d5f958af27110e8
Yafira <yafira.martinez@lc.cuny.edu>
2022-05-13 22:35:16 -0400

    added Pet component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+const Pet = () => {
+	return React.createElement('div', {}, [
+		React.createElement('h1', {}, 'Luna'),
+		React.createElement('h2', {}, 'Cat'),
+		React.createElement('h2', {}, 'Egyptian Mau'),
+	])
+}
+
 const App = () => {
-	return React.createElement(
-		'div',
-		{},
-		React.createElement('h1', {}, 'Adopt Me!')
-	)
+	return React.createElement('div', {}, [
+		React.createElement('h1', {}, 'Adopt Me!'),
+		React.createElement(Pet),
+		React.createElement(Pet),
+		React.createElement(Pet),
+	])
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
