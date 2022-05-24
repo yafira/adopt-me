c284ac6abbf3b682ac7c47269f8603e378127473
Yafira <yafira.martinez@lc.cuny.edu>
2022-05-23 20:53:51 -0400

    setting up Prettier

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 const Pet = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Luna'),
-		React.createElement('h2', {}, 'Cat'),
-		React.createElement('h2', {}, 'Egyptian Mau'),
-	])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h2", {}, "Cat"),
+    React.createElement("h2", {}, "Egyptian Mau"),
+  ]);
+};
 
 const App = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me!'),
-		React.createElement(Pet),
-		React.createElement(Pet),
-		React.createElement(Pet),
-	])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
