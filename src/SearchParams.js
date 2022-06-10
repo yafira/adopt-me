d0b352fb57282b7beeb2bc84f6be2c08d90df323
Yafira <yafira.martinez@lc.cuny.edu>
2022-06-09 22:10:48 -0400

    using useState hook to update the previous state

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "NYC, NY";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
