f0f9fcc01491424f0581c92bdd1da6d4adaa8952
Yafira <yafira.martinez@lc.cuny.edu>
2022-06-11 15:58:34 -0400

    minor edits

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import useBreedList from "./useBreedList";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
