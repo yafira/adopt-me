4154035ebf05fcc2f42ffd05b3021dd178089b2d
Yafira <yafira.martinez@lc.cuny.edu>
2022-06-14 20:53:54 -0400

    implementing React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { StrictMode } from "react";
 import SearchParams from "./SearchParams.js";
+import Details from "./Details.js";
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
+      <BrowserRouter>
         <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>hi lol omg bbq</h2>;
+};
+
+export default Details;
