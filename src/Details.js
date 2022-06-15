df33d134a41f09e4376043cb909bbc7ed94ef302
Yafira <yafira.martinez@lc.cuny.edu>
2022-06-14 23:14:25 -0400

    class properties + managing state in class components

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,16 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = { loading: true };
-  }
+  //   this.state = { loading: true };
+  // }
+
+  // class property, same as using constructor
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -22,10 +26,12 @@ class Details extends Component {
       return <h2>loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
