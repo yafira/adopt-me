30ea81b58ab08e6ea3249489a38741fdda3ec9e8
Yafira <yafira.martinez@lc.cuny.edu>
2022-06-14 22:23:27 -0400

    fetching data in componentDidMount

--- src/Details.js
+++ src/Details.js
@@ -8,6 +8,15 @@ class Details extends Component {
     this.state = { loading: true };
   }
 
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
   render() {
     if (this.state.loading) {
       return <h2>loading...</h2>;
@@ -30,4 +39,9 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
